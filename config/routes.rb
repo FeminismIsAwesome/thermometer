fea2bab8741b691216dc7ef10e755fbd57ef4bd8
ian.norris <ian.norris@Ian-Norris.local>
2016-04-22 14:42:36 -0700

    totally changing things

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'silly/:url' => "goofy#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
