c6dc23f9cd4d9f9707a696ab40793e3a2ec96ea3
ian.norris <ian.norris@Ian-Norris.local>
2016-04-26 10:48:31 -0700

    Revert "WHY U MAD"
    This reverts commit fd3a6a3c147d1cd5ad199001866451f0b164c249.

--- config/application.rb
+++ config/application.rb
@@ -28,15 +28,15 @@ module MySilly
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    # config.middleware.insert_before 0, "Rack::Cors" do
-    #   allow do
-    #     origins '*'
-    #     resource '*', :headers => :any, :methods => [:get, :post, :options]
-    #   end
-    #   allow do
-    #     origins '*'
-    #     resource 'silly', :headers => :any, :methods => [:get, :post, :options]
-    #   end
-    # end
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+      allow do
+        origins '*'
+        resource 'silly', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
